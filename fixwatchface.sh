d3ddcd0dee58af6518f6a88742052bd0eadfed71
Edward <oakeley@users.noreply.github.com>
2023-01-16 23:50:04 +0100

    Fixed to permit live updates for GTR3pro

--- fixwatchface.sh
+++ fixwatchface.sh
@@ -12,66 +12,84 @@
 # Set variables
 ##   Input watch
 export iX=336
-export oX=416
 export iY=384
-export oY=416
+
+name=falcon
+#export oX=416
+#export oY=416
+
+name=gtr-3-pro
+export oX=480
+export oY=480
+#rat=`echo "480/416" | awk '{A=split($1,a,"/"); print a[1]/a[2]}'`
+
 ##   Scaling factors
 xScale=`echo $iX" "$oX | awk '{print $2/$1}'`
 yScale=`echo $iY" "$oY | awk '{print $2/$1}'`
 
 # Date position
-export dPos=105
+export dPos=95
 if [[ $1 == "bot" ]]; then dPos=345; fi
 
+#dPos=`echo $dPos $rat | awk '{print int(($1*$2)+.5)}'`
+
 # Backup all the folders and copy the gts4mini into falcon so we can have the updates
-rm -rf zeppos_watchdrip_timer_wf/assets/falcon  # zeppos_watchdrip_timer_wf/assets/oldfalcon
-rm -rf zeppos_watchdrip_timer_wf/watchface/falcon  #zeppos_watchdrip_timer_wf/watchface/oldfalcon
-rsync -vaz zeppos_watchdrip_timer_wf/assets/gts4mini/ zeppos_watchdrip_timer_wf/assets/falcon
-rsync -vaz zeppos_watchdrip_timer_wf/watchface/gts4mini/ zeppos_watchdrip_timer_wf/watchface/falcon
+rm -rf zeppos_watchdrip_timer_wf/assets/$name
+rm -rf zeppos_watchdrip_timer_wf/watchface/$name
+rsync -vaz zeppos_watchdrip_timer_wf/assets/gts4mini/ zeppos_watchdrip_timer_wf/assets/$name
+rsync -vaz zeppos_watchdrip_timer_wf/watchface/gts4mini/ zeppos_watchdrip_timer_wf/watchface/$name
 
 # Make a Falcon low power background
-cp ~/Pictures/bg.png zeppos_watchdrip_timer_wf/assets/falcon/images/bg/
+cp ~/Pictures/bg-"$name".png zeppos_watchdrip_timer_wf/assets/$name/images/bg/bg.png
 # Make a low power heart icon
-cp ~/Pictures/heart.png zeppos_watchdrip_timer_wf/assets/falcon/images/widgets/
+cp ~/Pictures/heart.png zeppos_watchdrip_timer_wf/assets/$name/images/widgets/
 # Fix the glucose warning icons
-cp ~/Pictures/bg?*.png zeppos_watchdrip_timer_wf/assets/falcon/watchdrip/
+cp ~/Pictures/bg?*.png zeppos_watchdrip_timer_wf/assets/$name/watchdrip/
 # Fix the preview image
-cp ~/Pictures/preview.png zeppos_watchdrip_timer_wf/assets/falcon/images/
+cp ~/Pictures/preview.png zeppos_watchdrip_timer_wf/assets/$name/images/
 
-# Apply the screen calculations to every number that looks like a coordinate write intermetiate tmp file to RAM
+# Auto-fixing doesn't actually work that well because it is so hard to make a general rule so we just edit the rest inline and write intermetiate tmp file to RAM
 cat zeppos_watchdrip_timer_wf/watchface/gts4mini/styles.js | \
-   tr "(" "@" | \
-   awk -v xS=$xScale -v yS=$yScale -F"px@" '{OFS="px@"; if ($0 ~ "px@") {A=split($2,a,")"); if (length(a[1])<4) {if ($0 ~ "x: px@" || $0 ~ "X: px@" || $0 ~ "w: px@") {x=int((a[1]*xS)+.5); $2=x")"a[2]}; if ($0 ~ "y: px@" || $0 ~ "Y: px@" || $0 ~ "h: px@") {y=int((a[1]*yS)+.5); $2=y")"a[2]}}}; print $0 }' | \
-   tr "@" "(" > /dev/shm/styles.js.tmp
+   sed "s/hour_startX: px(.*),/hour_startX: +("`echo 110 $xScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
+   sed "s/hour_startY: px(.*),/hour_startY: +("`echo 26 $yScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
+   sed "s/month_unit_sc: img(/\/\/ month_unit_sc: img(/g" | \
+   sed "s/month_unit_tc: img(/\/\/ month_unit_tc: img(/g" | \
+   sed "s/month_unit_en: img(/\/\/ month_unit_en: img(/g" | \
+   sed "s/    x: px(5),/    x: +("`echo 10 $xScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
+   sed "s/const dateX = px(163);/const dateX = +("`echo 163 $xScale | awk '{print int(($1*$2)+.5)}'`");/g" | \
+   sed "s/const dateY = px(75);/const dateY = +("`echo $dPos $yScale | awk '{print int(($1*$2)+.5)}'`");/g" | \
+   sed "s/    y: px(81),/    y: +("`echo $dPos $yScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
+   sed "s/    x: px(125),/    x: +("`echo 110 $xScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
+   sed "s/    w: px(336),/    w: +("`echo 336 $xScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
+   sed "s/    h: px(384),/    h: +("`echo 384 $yScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
+   sed "s/const editWidgetW = px(90);/const editWidgetW = +("`echo 90 $xScale | awk '{print int(($1*$2)+.5)}'`");/g" | \
+   sed "s/const editWidgetH = px(70);/const editWidgetH = +("`echo 70 $yScale | awk '{print int(($1*$2)+.5)}'`");/g" | \
+   sed "s/const topLeftX = px(7);/const topLeftX = +("`echo 15 $xScale | awk '{print int(($1*$2)+.5)}'`");/g" | \
+   sed "s/const topLeftY = px(110);/const topLeftY = +("`echo 110 $yScale | awk '{print int(($1*$2)+.5)}'`");/g" | \
+   sed "s/const topRightX = px(239);/const topRightX = +("`echo 219 $xScale | awk '{print int(($1*$2)+.5)}'`");/g" | \
+   sed "s/const topRightY = px(110);/const topRightY = +("`echo 100 $xScale | awk '{print int(($1*$2)+.5)}'`");/g" | \
+   sed "s/const bottomLeftX = px(7);/const bottomLeftX = +("`echo 15 $xScale | awk '{print int(($1*$2)+.5)}'`");/g" | \
+   sed "s/const bottomLeftY = px(195);/const bottomLeftY = +("`echo 195 $yScale | awk '{print int(($1*$2)+.5)}'`");/g"| \
+   sed "s/const bottomRightX = px(239);/const bottomRightX = +("`echo 239 $xScale | awk '{print int(($1*$2)+.5)}'`");/g" | \
+   sed "s/const bottomRightY = px(195);/const bottomRightY = +("`echo 195 $yScale | awk '{print int(($1*$2)+.5)}'`");/g" | \
+   sed "s/    y: px(276),/    y: +("`echo 276 $yScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
+   sed "s/    x: px(101),/    x: +("`echo 115 $xScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
+   sed "s/    y: px(75),/    y: +("`echo $dPos $yScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
+   sed "s/    x: px(259),/    x: +("`echo 259 $xScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
+   sed "s/    y: px(276),/    y: +("`echo 276 $yScale | awk '{print int(($1*$2)+.5)}'`"),/g" \
+   > /dev/shm/styles.js.tmp
 
-# Auto-fixing doesn't actually work that well because it is so hard to make a general rule so we just edit the rest inline
+# Apply the screen calculations to every remaining number that looks like a coordinate
 cat /dev/shm/styles.js.tmp | \
-   sed 's/hour_startX: px(104),/hour_startX: px(124),/g' | \
-   sed 's/hour_startY: px(6),/hour_startY: px(36),/g' | \
-   sed 's/month_unit_sc: img(/\/\/ month_unit_sc: img(/g' | \
-   sed 's/month_unit_tc: img(/\/\/ month_unit_tc: img(/g' | \
-   sed 's/month_unit_en: img(/\/\/ month_unit_en: img(/g' | \
-   sed 's/const dateX = px(163);/const dateX = px(203);/g' | \
-   sed "s/const dateY = px(75);/const dateY = px("$dPos");/g" | \
-   sed "s/    y: px(81),/    y: px("$dPos"),/g" |\
-   sed 's/    x: px(125),/    x: px(145),/g' | \
-   sed 's/    w: px(336),/    w: px(416),/g' | \
-   sed 's/    h: px(384),/    h: px(416),/g' | \
-   sed 's/const editWidgetW = px(90);/const editWidgetW = px(105);/g' | \
-   sed 's/const editWidgetH = px(70);/const editWidgetH = px(76);/g' | \
-   sed 's/const topLeftX = px(7);/const topLeftX = px(32);/g' | \
-   sed 's/const topLeftY = px(110);/const topLeftY = px(115);/g' | \
-   sed 's/const topRightX = px(239);/const topRightX = px(286);/g' | \
-   sed 's/const bottomLeftX = px(7);/const bottomLeftX = px(42);/g' | \
-   sed 's/const bottomLeftY = px(195);/const bottomLeftY = px(217);/g'| \
-   sed 's/const bottomRightX = px(239);/const bottomRightX = px(286);/g' | \
-   sed 's/const bottomRightY = px(195);/const bottomRightY = px(217);/g' \
-   > zeppos_watchdrip_timer_wf/watchface/falcon/styles.js
+   tr "(" "@" | \
+   awk -v xS1=$xScale -v yS1=$yScale -F"px@" '{OFS="px@"; xS=xS1*.95; yS=yS1*.95; if ($0 ~ "px@") {A=split($2,a,")"); if (length(a[1])<4) {if ($0 ~ "x: px@" || $0 ~ "X: px@" || $0 ~ "w: px@") {x=int((a[1]*xS)+.5); $2=x")"a[2]}; if ($0 ~ "y: px@" || $0 ~ "Y: px@" || $0 ~ "h: px@") {y=int((a[1]*yS)+.5); $2=y")"a[2]}}}; print $0 }' | \
+   tr "@" "(" | \
+   sed 's/ +(/ px(/g' > zeppos_watchdrip_timer_wf/watchface/$name/styles.js
 
 if [[ $1 != "bot" ]]; then exit; fi
 
 # Optionally get rid of AAPS stuff because it doesn't work with default xDrip
-perl -0777 -i -pe 's/\QFill data from modified xDrip ExternalStatusService.getLastStatusLine\E.*?\QaapsText.setProperty(hmUI.prop.TEXT, aapsString);\E//gs' zeppos_watchdrip_timer_wf/watchface/falcon/index.js
+#perl -0777 -i -pe 's/\QFill data from modified xDrip ExternalStatusService.getLastStatusLine\E.*?\QaapsText.setProperty(hmUI.prop.TEXT, aapsString);\E//gs' zeppos_watchdrip_timer_wf/watchface/$name/index.js
 
-
-
+#   sed "s/    x: px(259),/    x: px("`echo 259 $xScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
+#   sed "s/    x: px(5),/    x: px("`echo 5 $xScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \   
