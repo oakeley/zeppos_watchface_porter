3afba253f303de83222884933d20129080c9edd5
Edward <oakeley@users.noreply.github.com>
2023-01-17 23:02:48 +0100

    Changed syntax to watchname

--- fixwatchface.sh
+++ fixwatchface.sh
@@ -4,24 +4,18 @@
 ##   mkdir folder
 ##   cd folder
 ##   git clone https://github.com/4RK4N/zeppos_watchdrip_timer_wf
-## To remove AAPS text and replace with date:
-##   bash fixwatchface.sh bot
-## To keep AAPS text and squish the date in below the clock:
-##   bash fixwatchface.sh top
+##   bash fixwatchface.sh falcon (allowed options are "falcon" or gtr-3-pro"... ping me on Discord with the screen size if you have a different watch)
 
 # Set variables
 ##   Input watch
 export iX=336
 export iY=384
 
-name=falcon
-#export oX=416
-#export oY=416
+d="F"
 
-name=gtr-3-pro
-export oX=480
-export oY=480
-#rat=`echo "480/416" | awk '{A=split($1,a,"/"); print a[1]/a[2]}'`
+if [[ $1 == "falcon" ]]; then name=falcon; export oX=416; export oY=416; offset=1; dO=101 d="T"; fi
+if [[ $1 == "gtr-3-pro" ]]; then name=gtr-3-pro; export oX=480; export oY=480; offset=0.95; d0=110; d="T"; fi
+if [[ $d == "F" ]]; then echo "Unsupported watch"; exit; fi
 
 ##   Scaling factors
 xScale=`echo $iX" "$oX | awk '{print $2/$1}'`
@@ -29,7 +23,7 @@ yScale=`echo $iY" "$oY | awk '{print $2/$1}'`
 
 # Date position
 export dPos=95
-if [[ $1 == "bot" ]]; then dPos=345; fi
+#if [[ $1 == "bot" ]]; then dPos=345; fi
 
 #dPos=`echo $dPos $rat | awk '{print int(($1*$2)+.5)}'`
 
@@ -50,7 +44,7 @@ cp ~/Pictures/preview.png zeppos_watchdrip_timer_wf/assets/$name/images/
 
 # Auto-fixing doesn't actually work that well because it is so hard to make a general rule so we just edit the rest inline and write intermetiate tmp file to RAM
 cat zeppos_watchdrip_timer_wf/watchface/gts4mini/styles.js | \
-   sed "s/hour_startX: px(.*),/hour_startX: +("`echo 110 $xScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
+   sed "s/hour_startX: px(.*),/hour_startX: +("`echo $d0 $xScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
    sed "s/hour_startY: px(.*),/hour_startY: +("`echo 26 $yScale | awk '{print int(($1*$2)+.5)}'`"),/g" | \
    sed "s/month_unit_sc: img(/\/\/ month_unit_sc: img(/g" | \
    sed "s/month_unit_tc: img(/\/\/ month_unit_tc: img(/g" | \
@@ -82,11 +76,11 @@ cat zeppos_watchdrip_timer_wf/watchface/gts4mini/styles.js | \
 # Apply the screen calculations to every remaining number that looks like a coordinate
 cat /dev/shm/styles.js.tmp | \
    tr "(" "@" | \
-   awk -v xS1=$xScale -v yS1=$yScale -F"px@" '{OFS="px@"; xS=xS1*.95; yS=yS1*.95; if ($0 ~ "px@") {A=split($2,a,")"); if (length(a[1])<4) {if ($0 ~ "x: px@" || $0 ~ "X: px@" || $0 ~ "w: px@") {x=int((a[1]*xS)+.5); $2=x")"a[2]}; if ($0 ~ "y: px@" || $0 ~ "Y: px@" || $0 ~ "h: px@") {y=int((a[1]*yS)+.5); $2=y")"a[2]}}}; print $0 }' | \
+   awk -v xS1=$xScale -v yS1=$yScale -v of=$offset -F"px@" '{OFS="px@"; xS=xS1*of; yS=yS1*of; if ($0 ~ "px@") {A=split($2,a,")"); if (length(a[1])<4) {if ($0 ~ "x: px@" || $0 ~ "X: px@" || $0 ~ "w: px@") {x=int((a[1]*xS)+.5); $2=x")"a[2]}; if ($0 ~ "y: px@" || $0 ~ "Y: px@" || $0 ~ "h: px@") {y=int((a[1]*yS)+.5); $2=y")"a[2]}}}; print $0 }' | \
    tr "@" "(" | \
    sed 's/ +(/ px(/g' > zeppos_watchdrip_timer_wf/watchface/$name/styles.js
 
-if [[ $1 != "bot" ]]; then exit; fi
+#if [[ $1 != "bot" ]]; then exit; fi
 
 # Optionally get rid of AAPS stuff because it doesn't work with default xDrip
 #perl -0777 -i -pe 's/\QFill data from modified xDrip ExternalStatusService.getLastStatusLine\E.*?\QaapsText.setProperty(hmUI.prop.TEXT, aapsString);\E//gs' zeppos_watchdrip_timer_wf/watchface/$name/index.js
